140ec07380030b0a7e443c3d6b1e1d69ab60ed05
TakemotoYuika <k19rs074@st.kyusan-u.ac.jp>
2021-12-16 09:55:14 +0900

    APIキーの設定をけしました

--- www/css/keys.js
+++ www/css/keys.js
@@ -1,4 +1,1 @@
-// This is a JavaScript file
-    // APIキーの設定
-    var APPLICATION_KEY ="dfdfc0bac66f1dca87855909f1a7189761e3b282e06c15131e703a1920ff84c1";
-    var CLIENT_KEY ="563f97bbd0c8c32eec58c71fb8808014cf837f02a889245f46b78b9ef8128f9b";
+
